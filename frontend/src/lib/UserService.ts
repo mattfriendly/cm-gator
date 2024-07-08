89c0d6f60e9529f519ee98f8105e3dd12b320d53
Arnav Surve <arnav.surve@milgard.com>
2024-07-08 10:26:13 -0700

    feat: Add getUsers function to fetch and display a list of users

---
+++ frontend/src/lib/UserService.ts
@@ -0,0 +1,18 @@
+export async function getUsers() {
+    try {
+        const response = await fetch('https://127.0.0.1:8443/listUsers', { // edit url to match backend
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                // add req headers here
+            }
+        });
+        if (!response.ok) {
+            throw new Error('Failed to get user');
+        }
+        return await response.json();
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
